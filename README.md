28972d31171f208230383149614fcc416b9c5397
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2016-11-21 14:26:21 +0000

    First func - server

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ggshell
-Gregor Gorjanc's shell functions and scripts
+Gregor Gorjanc's (bash) shell functions and scripts
+
+## Description
+
+A set of functions and scripts that make my life easier when working in shell/terminal.
+
+Functions are stored in files named *func_foo.sh* while scripts are stored in
+files named *foo.sh*.
+
+## Instalation
+
+1. Save the files in your ~/bin folder
+2. Add ~/bin folder to your PATH variable, i.e., export PATH=~/bin:$PATH
+3. Add the following code to your ~/.bashrc file for each function you want to use
+
+if [ -f ~/bin/func_foo.sh ]; then
+  source ~/bin/func_foo.sh
+fi
+
---
+++ func_server.sh
@@ -0,0 +1,78 @@
+
+# SERVER
+#-------------------------------------------------------------------------------
+# What: Set server specific aliases and functions
+# Who: Gregor Gorjanc
+# When:
+#  * 2014-05 Added the diff functions
+#  * 2013-12 Initial version
+#-------------------------------------------------------------------------------
+
+server()
+{
+  # $1 = server name abbreviation
+  # $2 = server IP address
+  # $3 = username
+  # $4 = SSH options
+
+  optSSH=$(echo ${4} | sed -e "s/-P/-p/")
+  optSCP=$(echo ${4} | sed -e "s/-p/-P/")
+
+  eval "alias ping${1}=\"ping ${2}\""
+  eval "alias login${1}=\"ssh ${optSSH} ${3}@${2}\""
+
+  eval "pull${1}()
+  {
+    local i
+    for i in \$@; do
+      scp -r ${optSCP} ${3}@${2}:\$i .
+    done
+  }; export -f pull${1}"
+
+  eval "push${1}()
+  {
+    echo "TODO: how to add an option to push to a user defined folder instead of ~/."
+    scp -r ${optSCP} \$@ ${3}@${2}:~/.
+  }; export -f push${1}"
+
+  eval "diff${1}()
+  {
+    DIFF=diff
+    case \${1} in
+      --diff=* )
+        DIFF=\$(echo \${1} | sed -e \"s/--diff=//\")
+        shift
+        ;;
+    esac
+    file=\$(basename \${1})
+    scp ${optSCP} ${3}@${2}:\${1} \${file}_from_${1}
+    if [ -z \"\${2}\" ]; then
+      trg=\${file}
+    else
+      trg=\${2}
+    fi
+    echo \"Compare \${file}_from_${1} to \${trg} (12=default) or vice versa (21)?\"
+    read WAY
+    if [ \"\${WAY}\" == \"21\" ]; then
+      \${DIFF} \${trg} \${file}_from_${1}
+    else
+      \${DIFF} \${file}_from_${1} \${trg}
+    fi
+    echo \"\nRemove the copied file \${file}_from_${1} (Y/n)?\"
+    read YORN
+    if [ \"\${YORN}\" != \"n\" ]; then
+       rm -f \${file}_from_${1}
+    fi
+  }; export -f diff${1}"
+
+  eval "cwdiff${1}()
+  {
+    diff${1} --diff=cwdiff \${@}
+  }; export -f cwdiff${1}"
+
+  eval "colordiff${1}()
+  {
+    diff${1} --diff=colordiff \${@}
+  }; export -f colordiff${1}"
+}
+export -f server
